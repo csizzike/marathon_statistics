abd10e92c417ee7f7c8280baa92485c1380eed39
csizzike <43538455+csizzike@users.noreply.github.com>
2021-11-21 22:46:43 +0100

    timedelta issue commented out

--- marathon_statistics_app.py
+++ marathon_statistics_app.py
@@ -26,18 +26,18 @@ def load_data(year):
     bszm.drop(['1.nap/1.idõ', '1.nap/2.idõ', '1.nap/3.idõ', '2.nap/1.idõ', '2.nap/2.idõ', '2.nap/3.idõ', '3.nap /1.idõ', '3.nap/2.idõ', '3.nap/3.idõ', '4.nap/1.idõ', '4.nap/2.idõ', '4.nap/3.idõ'], axis = 1, inplace=True)
     bszm["befejezte"] = np.where(bszm['Megtett táv (km)'] < 196, False, True)
     bszm = bszm.replace('99:59:59', np.nan)
-    bszm['eredmény'] = pd.to_timedelta(bszm['eredmény'])
-    bszm['1.nap összidõ'] = pd.to_timedelta(bszm['1.nap összidõ'])
-    bszm['2.nap összidõ'] = pd.to_timedelta(bszm['2.nap összidõ'])
-    bszm['3.nap összidõ'] = pd.to_timedelta(bszm['3.nap összidõ'])
-    bszm['4.nap összidõ'] = pd.to_timedelta(bszm['4.nap összidõ'])
-    eredmény = ['1.nap összidõ', '2.nap összidõ', '3.nap összidõ', '4.nap összidõ']
-    bszm['eredmény'] = bszm[eredmény].sum(axis=1, skipna=True)
-    bszm['eredmény'] = bszm['eredmény'] - pd.to_timedelta(bszm['eredmény'].dt.days, unit='h')
-    bszm["eredmény(perc)"] = bszm['eredmény'].astype('timedelta64[m]')
-    bszm["átlagos tempó (perc/km)"] = bszm['eredmény'].astype('timedelta64[m]')/bszm['Megtett táv (km)']
-    bszm['sebesség kategória'] = pd.qcut(bszm['átlagos tempó (perc/km)'], 5, labels=['gyors', 'közepesen gyors', 'közepes', 'közepesen lassú', 'lassú'])
-    bszm.drop_duplicates(inplace=True)
+    #bszm['eredmény'] = pd.to_timedelta(bszm['eredmény'])
+    #bszm['1.nap összidõ'] = pd.to_timedelta(bszm['1.nap összidõ'])
+    #bszm['2.nap összidõ'] = pd.to_timedelta(bszm['2.nap összidõ'])
+    #bszm['3.nap összidõ'] = pd.to_timedelta(bszm['3.nap összidõ'])
+    #bszm['4.nap összidõ'] = pd.to_timedelta(bszm['4.nap összidõ'])
+    #eredmény = ['1.nap összidõ', '2.nap összidõ', '3.nap összidõ', '4.nap összidõ']
+    #bszm['eredmény'] = bszm[eredmény].sum(axis=1, skipna=True)
+    #bszm['eredmény'] = bszm['eredmény'] - pd.to_timedelta(bszm['eredmény'].dt.days, unit='h')
+    #bszm["eredmény(perc)"] = bszm['eredmény'].astype('timedelta64[m]')
+    #bszm["átlagos tempó (perc/km)"] = bszm['eredmény'].astype('timedelta64[m]')/bszm['Megtett táv (km)']
+    #bszm['sebesség kategória'] = pd.qcut(bszm['átlagos tempó (perc/km)'], 5, labels=['gyors', 'közepesen gyors', 'közepes', 'közepesen lassú', 'lassú'])
+    #bszm.drop_duplicates(inplace=True)
     #bszm.loc[bszm['eredmény(perc)'] == 0.0] = np.nan
     bszm.dropna(axis=0, how='all', inplace=True)
     return bszm
@@ -45,6 +45,6 @@ def load_data(year):
 df = load_data(selected_year)
 
 st.header('Résztvevők eredményei')
-st.write('Dimenziók: ' + str(df.shape[0]) + 'sor és ' + str(df.shape[1]) + ' oszlop')
+st.write('Dimenziók: ' + str(df.shape[0]) + ' sor és ' + str(df.shape[1]) + ' oszlop')
 st.dataframe(df)
     
