259df9f6e0a13efd4000261338ab3a88373bb7d0
csizzike <43538455+csizzike@users.noreply.github.com>
2021-11-21 15:09:36 +0100

    Add sidebar selectbox

--- marathon_statistics_app.py
+++ marathon_statistics_app.py
@@ -11,3 +11,6 @@ summary_md = str(open("README.md").read())
 st.title('BSZM egyéni eredmény kiértékelés')
 
 st.markdown(summary_md)
+
+st.sidebar.header('Év kiválasztása')
+selected_year = st.sidebar.selectbox('Év', list(reversed(range(2015,2020))))
