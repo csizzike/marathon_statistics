4efc2b96762890d1686efe28a52f43e0c3b063d7
csizzike <43538455+csizzike@users.noreply.github.com>
2021-11-22 13:58:30 +0100

    asd

--- marathon_statistics_app.py
+++ marathon_statistics_app.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import Inf, NaN
 import streamlit as st
 import pandas as pd
 import base64
@@ -11,21 +12,24 @@ static_path = './dataset/csv/'
 
 summary_md = str(open("README.md").read())
 
-st.title('BSZM egyéni eredmény kiértékelés')
+
+st.title('Marathon analysis')
 
 st.markdown(summary_md)
 
-st.sidebar.header('Év kiválasztása')
+st.sidebar.header('Select year')
 selected_year = st.sidebar.selectbox('Év', list(reversed(range(2015,2021))))
 
 @st.cache
 def load_data(year):
     bszm = pd.read_csv(static_path + 'BSZM_' + str(year) + '.csv', encoding = "ISO-8859-1", 
                     sep=';', header=0)
-    bszm.drop(['Név/Csapatnév', 'Ország', 'Csapat', 'Város', 'rajtszám', 'Hely.', 'kat.h.'], axis = 1, inplace=True)
-    bszm.drop(['1.nap/1.idõ', '1.nap/2.idõ', '1.nap/3.idõ', '2.nap/1.idõ', '2.nap/2.idõ', '2.nap/3.idõ', '3.nap /1.idõ', '3.nap/2.idõ', '3.nap/3.idõ', '4.nap/1.idõ', '4.nap/2.idõ', '4.nap/3.idõ'], axis = 1, inplace=True)
+    bszm['Hely.'] = bszm['Hely.'].replace(NaN, 9999)
+    bszm['Hely.'] = bszm['Hely.'].astype(int)
+    #bszm.drop(['Név/Csapatnév', 'Ország', 'Csapat', 'Város', 'rajtszám', 'Hely.', 'kat.h.'], axis = 1, inplace=True)
+    #bszm.drop(['1.nap/1.idõ', '1.nap/2.idõ', '1.nap/3.idõ', '2.nap/1.idõ', '2.nap/2.idõ', '2.nap/3.idõ', '3.nap /1.idõ', '3.nap/2.idõ', '3.nap/3.idõ', '4.nap/1.idõ', '4.nap/2.idõ', '4.nap/3.idõ'], axis = 1, inplace=True)
     bszm["befejezte"] = np.where(bszm['Megtett táv (km)'] < 196, False, True)
-    bszm = bszm.replace('99:59:59', np.nan)
+    #bszm = bszm.replace('99:59:59', np.nan)
     #bszm['eredmény'] = pd.to_timedelta(bszm['eredmény'])
     #bszm['1.nap összidõ'] = pd.to_timedelta(bszm['1.nap összidõ'])
     #bszm['2.nap összidõ'] = pd.to_timedelta(bszm['2.nap összidõ'])
@@ -37,7 +41,7 @@ def load_data(year):
     #bszm["eredmény(perc)"] = bszm['eredmény'].astype('timedelta64[m]')
     #bszm["átlagos tempó (perc/km)"] = bszm['eredmény'].astype('timedelta64[m]')/bszm['Megtett táv (km)']
     #bszm['sebesség kategória'] = pd.qcut(bszm['átlagos tempó (perc/km)'], 5, labels=['gyors', 'közepesen gyors', 'közepes', 'közepesen lassú', 'lassú'])
-    #bszm.drop_duplicates(inplace=True)
+    bszm.drop_duplicates(inplace=True)
     #bszm.loc[bszm['eredmény(perc)'] == 0.0] = np.nan
     bszm.dropna(axis=0, how='all', inplace=True)
     return bszm
